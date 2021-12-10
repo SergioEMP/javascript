f3f4805f4cfebe3c1f1db7bd400ff1bb2553d902
SergioEMP <sergio.martinez@nivelics.co>
2021-12-10 14:38:32 -0500

    Slider al 80%

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,11 @@
-$y(document).ready(function(){
+$(document).ready(function(){
+    $('.slider1').slick({
+        dots: true,
+        arrows:true
+    });
+  });
+
+$(document).ready(function(){
     $('.slider2').slick({
         dots: true,
         arrows:false
