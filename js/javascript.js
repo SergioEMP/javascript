83a170a018efe347fc412e5b2f1489002b8ea453
SergioEMP <sergio.martinez@nivelics.co>
2021-12-15 11:03:35 -0500

    Slider con estilos

--- js/javascript.js
+++ js/javascript.js
@@ -3,11 +3,22 @@ $(document).ready(function(){
         dots: true,
         arrows:true
     });
+    $('.slider2').slick({
+      dots: true,
+      arrows: true,
+    });
   });
 
-$(document).ready(function(){
-    $('.slider2').slick({
-        dots: true,
-        arrows: true,
-    });
+  var $status = $('.current');
+  var $slickElement = $('.slideshow');
+  
+  $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text(i + '/' + slick.slideCount);
+  });
+  
+  $slickElement.slick({
+    autoplay: true,
+    dots: true
   });
